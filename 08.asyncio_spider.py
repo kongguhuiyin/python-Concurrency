0ec6053f27aa58badfed1863d9b147d135e774c5
kongguhuiyin <50992415+kongguhuiyin@users.noreply.github.com>
2023-02-28 14:17:51 +0800

    Add files via upload

---
+++ 08.asyncio_spider.py
@@ -0,0 +1,23 @@
+import time
+import asyncio
+import aiohttp
+import blog_spider
+
+# 单线程异步IO
+async def async_spider(url):    # 协程
+	print(f"crawl url: {url}")
+	async with aiohttp.ClientSession() as session:   # 创建对象
+		async with session.get(url) as resp:
+			result = await resp.text()
+			print(f"crawl url: {url}, {len(result)}")
+
+
+loop = asyncio.get_event_loop()   # 超级循环
+
+tasks = [loop.create_task(async_spider(url)) for url in blog_spider.urls]
+
+start = time.time()
+loop.run_until_complete(asyncio.wait(tasks))
+end = time.time()
+print("use time:", end - start)
+
